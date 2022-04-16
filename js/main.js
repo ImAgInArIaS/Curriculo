b575eb77474018c1d99d25881fe4912d13f02d12
113127 Javier Soria <imaginativo@gmail.com>
2022-04-16 05:29:37 -0300

    mas actualizaciones

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
 //Swal.fire("Esta a punto de ver la entrega de Javier Soria Legajo 113127 Para la materia Programacion III de la tecnicatura universitara en programacion");
 
 $(document).ready(function () {
-  $("#contenedor").fadeIn(2500);
-  $("#flip").click(function () {
-    $("#panel").slideToggle("slow");
-  });
+$("#contenedor").fadeIn(2500);
+$("#flip").click(function () {
+$("#panel").slideToggle("slow");
+});
 });
