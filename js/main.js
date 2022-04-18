12dc1c3e2782b3fa81f49ebd92a8cd0c4eea5919
113127 Javier Soria <imaginativo@gmail.com>
2022-04-18 01:31:41 -0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
-alert("Esta a punto de ver la entrega de Javier Soria Legajo 113127 Para la materia Programacion III de la tecnicatura universitara en programacion, esta alerta no es recomdendable para esta clase de web solo es con fines de mostrar la alerta");
-
+//alert("Esto puede ser una carta de presentacion o un mensaje para que el profe sepa que el cv pertenece al alumno Javier Soria Legajo 113127");
+//Esta desactivada la alerta por que no es algo recomendable para un cv
 $(document).ready(function () {
 $("#contenedor").fadeIn(2500);
 $("#flip").click(function () {
